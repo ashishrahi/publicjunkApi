7b8ac9a7d54d2ee514325dd49549b70dd1065fb1
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2024-08-30 15:16:16 +0530

    first commit

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ require('dotenv').config();
 connectDB(); 
 //configuration
 const app = express();
-const port = process.env.PORT;
+const port = 5400;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
